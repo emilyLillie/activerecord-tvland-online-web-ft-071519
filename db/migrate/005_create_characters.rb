a296fcb98fb31701edf1d2059abc6c024e320f2c
Emily Lillie <emilylillie3@gmail.com>
2019-09-03 23:39:12 +0000

    Automatically backed up by Learn

---
+++ db/migrate/005_create_characters.rb
@@ -0,0 +1,10 @@
+class CreateCharacters < ActiveRecord::Migration[5.2]
+  
+  create_table :characters do |t|
+    t.string :name 
+    t.integer :actor_id 
+    t.integer :show_id 
+  end 
+end 
+  
+end 
